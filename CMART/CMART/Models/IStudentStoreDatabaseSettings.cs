019a7b94e6a1acf5b044562138a97deba66b6a8e
yuvarajamineni <152278544+yuvarajamineni@users.noreply.github.com>
2025-09-21 19:36:04 +0530

    Delete CMART directory

--- CMART/CMART/Models/IStudentStoreDatabaseSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿ namespace StudentManagement.Models
-{
-    public interface IStudentStoreDatabaseSettings
-    {
-        string StudentCoursesCollectionName { get; set; }
-        string ConnectionString { get; set; }
-        string DatabaseName { get; set; }
-    }
-}
